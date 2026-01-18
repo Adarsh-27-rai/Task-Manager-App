862a367781a47270f764d240205bcd98d5259e8b
Adarsh Rai <adarshrai1408@gmail.com>
2026-01-18 19:09:51 +0530

    updated

--- Backend/Routes/tasks.js
+++ Backend/Routes/tasks.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Task = require("../models/task");
+const Task = require("../Models/task");
 const authMiddleware = require("../Middleware/authMiddleware");
 
 const router = express.Router();
