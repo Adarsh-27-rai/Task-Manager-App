4e41cb9a74d36dd88aff51066b3005db6f78afde
Adarsh Rai <adarshrai1408@gmail.com>
2026-02-06 01:13:01 +0530

    jwt auth

--- Frontend/src/components/protectedRoutes.jsx
+++ Frontend/src/components/protectedRoutes.jsx
@@ -1,10 +1,21 @@
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
+
+    const isTokenExpired = (token) => {
+        try {
+            const payload = JSON.parse(atob(token.split(".")[1]));
+            return payload.exp * 1000 < Date.now();
+        } catch {
+            return true;
+        }
+    };
+
     const token = localStorage.getItem("token");
 
-    if(!token) {
-        return <Navigate to="/" />
+    if (!token || isTokenExpired(token)) {
+        localStorage.removeItem("token");
+        return <Navigate to="/" />;
     }
 
     return children;
